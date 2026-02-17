cef0a15645e7f2cb003c58b2fcff7f0a4220c9a1
Peter Wooden <peter.r.wooden@gmail.com>
2026-02-17 23:01:53 +1100

    Clean generated artifacts and tighten ignore/build settings

--- apps/web/src/context.js
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from "react";
-export const RoomContext = createContext(undefined);
-export function useRoomContext() {
-    const ctx = useContext(RoomContext);
-    if (!ctx) {
-        throw new Error("RoomContext is missing");
-    }
-    return ctx;
-}
